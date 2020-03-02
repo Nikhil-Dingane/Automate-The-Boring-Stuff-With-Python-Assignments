71e3f7656bc0c73fc07615829103ce1768016161
Krishagni <nikhil@krishagni.com>
2020-03-02 17:35:05 +0530

    web scraping

---
+++ lucky.py
@@ -0,0 +1,15 @@
+# lucky.py - Opens several Google search results.
+
+import requests, sys, webbrowser, bs4
+
+print ('Googling...') 
+
+res = requests.get('http://google.com/search?q=' + ' '.join(sys.argv[1:]))
+res.raise_for_status()
+
+soup = bs4.BeautifulSoup(res.text, 'lxml')
+
+link_elems = soup.select('.r a')
+num_open = min(5, len(link_elems))
+for i in range(num_open):
+    webbrowser.open('http://google.com' + link_elems[i].get('href'))
